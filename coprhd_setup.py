4f289d0eeb7259f9184ca129ca4dbbdc77d49033
Curt Bruns <curt.e.bruns@intel.com>
2015-10-10 19:02:56 +0200

    Updated scripts and README

--- coprhd_setup.py
+++ coprhd_setup.py
@@ -3,6 +3,9 @@ import time
 import sys
 import os
 import re
+ 
+# Hack for limiting urllib3 warnins about unverified HTTPS requests
+env={'PYTHONWARNINGS':"ignore"}
 
 def init():
     password = os.getenv('VIPR_PASSWORD')
@@ -14,10 +17,10 @@ def init():
 
 def login():
     # First Logout
-    print pexpect.run('/opt/storageos/cli/bin/viprcli logout')
+    print pexpect.run('/opt/storageos/cli/bin/viprcli logout',env=env)
     
     # Login to ViprCLI
-    child = pexpect.spawn('/opt/storageos/cli/bin/viprcli authenticate -u root -d /tmp')
+    child = pexpect.spawn('/opt/storageos/cli/bin/viprcli authenticate -u root -d /tmp',env=env)
     child.logfile = sys.stdout
     password = os.getenv('VIPR_PASSWORD')
     child.expect('Password.*: ')
@@ -28,14 +31,14 @@ def login():
 
 def set_provider():
     # Check out Storage Providers
-    result = pexpect.run('/opt/storageos/cli/bin/viprcli storageprovider list')
+    result = pexpect.run('/opt/storageos/cli/bin/viprcli storageprovider list',env=env)
     test = re.search(r'NAME\s+INTERFACE', result)
     if test is not None:
         print "We have Storage Providers, bailing out!"
         print "Providers: %s" % result
         return(-1)
     else:
-        child = pexpect.spawn('/opt/storageos/cli/bin/viprcli storageprovider create -n ScaleIO -provip 10.0.0.37 -provport 22 -u vagrant -secondary_username admin -if scaleio')
+        child = pexpect.spawn('/opt/storageos/cli/bin/viprcli storageprovider create -n ScaleIO -provip 10.0.0.37 -provport 22 -u vagrant -secondary_username admin -if scaleio',env=env)
         child.logfile = sys.stdout
         child.expect('Enter password of the storage provider:')
         child.sendline('vagrant')
@@ -50,15 +53,15 @@ def set_provider():
         child.close()
 
 def create_va(network):
-    print pexpect.run('/opt/storageos/cli/bin/viprcli varray create -n ScaleIO_VA')
+    print pexpect.run('/opt/storageos/cli/bin/viprcli varray create -n ScaleIO_VA',env=env)
     command = '/opt/storageos/cli/bin/viprcli network update -varray_add ScaleIO_VA -n ' + network
-    print pexpect.run(command)
+    print pexpect.run(command,env=env)
 
 def get_network():
     # Retry if network isn't created yet
     for i in range (1,20):
         print "Retry is: %s" % i
-        results = pexpect.run('/opt/storageos/cli/bin/viprcli  network list')
+        results = pexpect.run('/opt/storageos/cli/bin/viprcli  network list',env=env)
         result = results.split()
         for entry in result:
             test = re.search(r'\w+-ScaleIONetwork', entry)
@@ -70,28 +73,23 @@ def get_network():
 
 def create_vp():
     print "Creating Virtual Pool"
-    results = pexpect.run('/opt/storageos/cli/bin/viprcli vpool create -systemtype scaleio -type block -n VP1 -protocol ScaleIO -va ScaleIO_VA -pt Thick -desc VP1')
+    results = pexpect.run('/opt/storageos/cli/bin/viprcli vpool create -systemtype scaleio -type block -n VP1 -protocol ScaleIO -va ScaleIO_VA -pt Thick -desc VP1',env=env)
     print "Results are: %s" % results
 
 def create_tenant():
     print "Creating Admin Tenant for OS integration"
-    results = pexpect.run('/opt/storageos/cli/bin/viprcli tenant create -n admin -domain lab')
-    print "Results are: %s" % results
-
-def create_project():
-    print "Creating Project for Admin Tenant"
-    results = pexpect.run('/opt/storageos/cli/bin/viprcli project create -n admin -tn admin')
+    results = pexpect.run('/opt/storageos/cli/bin/viprcli tenant create -n admin -domain lab',env=env)
     print "Results are: %s" % results
 
 def create_vol():
     print "Creating Volume"
-    results = pexpect.run('/opt/storageos/cli/bin/viprcli volume create -tenant admin -pr admin -name TestVol1 -size 1G -vpool VP1 -va ScaleIO_VA')
+    results = pexpect.run('/opt/storageos/cli/bin/viprcli volume create -tenant admin -pr admin -name TestVol1 -size 1G -vpool VP1 -va ScaleIO_VA',env=env)
     print "Results are: %s" % results
 
 def add_keystone_auth():
     password = 'nomoresecrete'
     print "Adding Keystone Authorization"
-    child = pexpect.spawn('/opt/storageos/cli/bin/viprcli authentication add-provider -configfile /home/vagrant/auth_config.cfg')
+    child = pexpect.spawn('/opt/storageos/cli/bin/viprcli authentication add-provider -configfile /home/vagrant/auth_config.cfg',env=env)
     child.logfile = sys.stdout
     child.expect('Enter password of the Key1:')
     child.sendline(password)
@@ -111,8 +109,8 @@ if __name__ == "__main__":
         sys.exit(-1)
     create_va(network)
     create_vp()
-    create_tenant()
-    create_project()
+    # These are done in OS_Integration Step
+#    create_tenant()
+#    create_project()
     create_vol()
-    add_keystone_auth()
 
