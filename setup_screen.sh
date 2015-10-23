efd765802fd59f49166ae994bdcc05e69bf41ddf
Curt Bruns <curt.e.bruns@intel.com>
2015-10-23 20:59:08 +0200

    Added setup_screen

---
+++ setup_screen.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Setup screen process to capture the logging of the CoprHD
+# modules so we can scan through them 
+NL=`echo -ne '\015'`
+SCREEN_NAME=${SCREEN_NAME:-coprhd}
+
+echo "Starting Screen"
+# Screen 0
+screen -d -m -S $SCREEN_NAME -t shell -s /bin/bash
+
+# Screens 1..x
+count=1
+for service in apisvc authsvc dbsvc controllersvc syssvc portalsvc
+do
+  screen -S $SCREEN_NAME -X screen -t $service
+  echo "Starting ${service}"
+  screen -S $SCREEN_NAME -p ${count} -X stuff "tail -f /opt/storageos/logs/${service}.log"$NL
+  count=$((count+1))
+done
+
+
